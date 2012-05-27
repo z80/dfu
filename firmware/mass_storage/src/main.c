9f573622e1325c2999c3fdcb89f9906395051327
z80 <litedictteam@gmail.com>
2012-05-27 22:48:19 +0400

    Optimzation off, FLASH initialization on.

--- firmware/mass_storage/src/main.c
+++ firmware/mass_storage/src/main.c
@@ -43,7 +43,7 @@ extern uint16_t MAL_Init (uint8_t lun);
 int main(void)
 {
   //NVIC_SetVectorTable( NVIC_VectTab_FLASH, 0x3000 );
-  //Set_System();
+  Set_System();
   Set_USBClock();
   Led_Config();
   USB_Interrupts_Config();
