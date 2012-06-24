91daba8c2136d180a4aaee9ea96b627e96e02e8d
z80 <litedictteam@gmail.com>
2012-06-24 20:27:36 +0400

    Testing io.

--- control-board/software/tests/test-usbio/src/main.cpp
+++ control-board/software/tests/test-usbio/src/main.cpp
@@ -4,12 +4,13 @@
 
 int main( int argc, char * argv[] )
 {
-    UsbIo io;
+    DeviceIo io;
     bool res = io.open();
     const int CNT = 64;
     unsigned char b[ CNT ];
-    int cnt = io.write( b, CNT );
-    cnt = io.read( b, CNT );
+    //int cnt = io.write( b, CNT );
+    //cnt = io.read( b, CNT );
+    int v = io.version();
     io.close();
     return 0;
 }
