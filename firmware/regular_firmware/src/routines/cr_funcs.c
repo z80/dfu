642a794168d29437734d6801bcc28937f473dc04
unknown <Sasha@Owner-VAIO.(none)>
2012-07-20 02:02:03 +0400

    Removed bad syntax.

--- firmware/regular_firmware/src/routines/cr_funcs.c
+++ firmware/regular_firmware/src/routines/cr_funcs.c
@@ -55,59 +55,57 @@ void crFuncs( xCoRoutineHandle xHandle,
         //if ( g_funcId != FUNC_IDLE )
         //    setRed( ( red() ) ? 0 : 1 );
         sendCnt = 0;
-        switch ( g_funcId )
+        if ( g_funcId == FUNC_VERSION )
         {
-            if ( g_funcId == FUNC_VERSION )
-	    {
-                res16 = VERSION;
-                out = (uint8_t *)&res16;
-                sendCnt = 2;
-            }
-            else if ( g_funcId == FUNC_GPIO_EN )
-	    {
-                gpioEn( buf[0], buf[1] );
-            }
-            else if ( g_funcId == FUNC_GPIO_CONFIG )
-	    {
-                gpioConfig( buf[0], 
-                            *(uint16_t *)(&(buf[1])), 
-                            buf[3] );
-            }
-            else if ( g_funcId == FUNC_GPIO_SET )
-	    {
-                gpioSet( buf[0], 
-                         *(uint16_t *)(&(buf[1])), 
-                         *(uint16_t *)(&(buf[3])) );
-            }
-            else if ( g_funcId == FUNC_GPIO )
-	    {
-                res16 = gpio( buf[0] ); 
-                out = (uint8_t *)&res16;
-                sendCnt = 2;
-            }
+            res16 = VERSION;
+            out = (uint8_t *)&res16;
+            sendCnt = 2;
+        }
+        else if ( g_funcId == FUNC_GPIO_EN )
+        {
+            gpioEn( buf[0], buf[1] );
+        }
+        else if ( g_funcId == FUNC_GPIO_CONFIG )
+        {
+            gpioConfig( buf[0],
+                        *(uint16_t *)(&(buf[1])),
+                        buf[3] );
+        }
+        else if ( g_funcId == FUNC_GPIO_SET )
+        {
+            gpioSet( buf[0],
+                     *(uint16_t *)(&(buf[1])),
+                     *(uint16_t *)(&(buf[3])) );
+        }
+        else if ( g_funcId == FUNC_GPIO )
+        {
+            res16 = gpio( buf[0] );
+            out = (uint8_t *)&res16;
+            sendCnt = 2;
+        }
 	    else if ( g_funcId == FUNC_I2C_STATUS )
 	    {
 	        uint8_t r = i2cStatus( buf[0] );
-		crQUEUE_SEND( xHandle, q, &r, 0, &cr );
+	        crQUEUE_SEND( xHandle, q, &r, 0, &cr );
 	    }
-            else if ( g_funcId == FUNC_I2C_EN )
-	    {
-                i2cSetEn( buf[0], buf[1] );
-            }
-            else if ( g_funcId == FUNC_I2C_CONFIG )
-	    {
-                i2cConfig( buf[0], buf[1], buf[2], *(uint32_t *)(&(buf[3])) );
-            }
-            else if ( g_funcId == FUNC_I2C_IO )
-	    {
-	        i2cIo( buf[0], buf[1], buf[2], buf[3], (uint8_t *)&(buf[4]) );
-            }
-	    else if ( g_funcId == FUNC_I2C_RESULT )
-	    {
-	        sendCnt = buf[1];
-		out = i2cReceiveQueue( buf[0] );
-	    }
+        else if ( g_funcId == FUNC_I2C_EN )
+        {
+            i2cSetEn( buf[0], buf[1] );
         }
+        else if ( g_funcId == FUNC_I2C_CONFIG )
+        {
+            i2cConfig( buf[0], buf[1], buf[2], *(uint32_t *)(&(buf[3])) );
+        }
+        else if ( g_funcId == FUNC_I2C_IO )
+        {
+	        i2cIo( buf[0], buf[1], buf[2], buf[3], (uint8_t *)&(buf[4]) );
+        }
+        else if ( g_funcId == FUNC_I2C_RESULT )
+        {
+        	sendCnt = buf[1];
+        	out = i2cReceiveQueue( buf[0] );
+        }
+
         static uint8_t i;
         for ( i=0; i<sendCnt; i++ )
             crQUEUE_SEND( xHandle, q, &out[i], 0, &cr );
