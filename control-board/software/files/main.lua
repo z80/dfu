8e7c3d7c7cc0a3954cb2798d0c817864650eba72
unknown <Sasha@Owner-VAIO.(none)>
2012-07-20 05:53:18 +0400

    Lua tests for I2C

--- control-board/software/files/main.lua
+++ control-board/software/files/main.lua
@@ -3,10 +3,10 @@ require( "debugger" )
 require( "ctrl_board" )
 require( "bit" )
 
-Joystick = class()
+I2c = class()
 
-function Joystick:__init()
-    self.wnd = qt.load_ui( "joystick.ui" )
+function I2c:__init()
+    self.wnd = qt.load_ui( "./i2c/i2c.ui" )
     self.wnd:show()
 
     self.dev = CtrlBoard()
@@ -15,46 +15,67 @@ function Joystick:__init()
     self.dev:gpioEn( 1, true )
     self.dev:gpioConfig( 1, 0xffff, self.dev.GPIO_IPD )
     --self.dev:gpioSet( 1, 65535, 65535 )
+    qt.connect( self.wnd.open,    'clicked', self, self.open )
+    qt.connect( self.wnd.close,   'clicked', self, self.close )
+    qt.connect( self.wnd.enable,  'clicked', self, self.enable )
+    qt.connect( self.wnd.disable, 'clicked', self, self.disable )
+    qt.connect( self.wnd.config,  'clicked', self, self.config )
+    qt.connect( self.wnd.send01,  'clicked', self, self.send01 )
+    qt.connect( self.wnd.send02,  'clicked', self, self.send02 )
+    qt.connect( self.wnd.send03,  'clicked', self, self.send03 )
 end
 
-function Joystick:refresh()
-    print( "Refresh" )
-    if ( self.dev:isOpen() ) then
-        local ver = self.dev:version()
-        print( "version = " .. tostring( ver ) )
-        local val = self.dev:gpio( 1 ) or 0
-        --val = bit.band( val, 2 + 4 + 1024 + 2048 )
-        local stri = bit.tohex( val )
-        self.wnd.hex:setText( stri )
-        stri = ""
-        if ( bit.band( val, 2 ) > 0 ) then
-            stri = stri .. "Up"
-        end
-        if ( bit.band( val, 4 ) > 0 ) then
-            stri = stri .. "Down"
-        end
-        if ( bit.band( val, 1024 ) > 0 ) then
-            stri = stri .. "Left"
-        end
-        if ( bit.band( val, 2048 ) > 0 ) then
-            stri = stri .. "Right"
-        end
-        self.wnd.text:setText( stri )
+function I2c:open()
+    self.dev:open()
+    print( "result: "  ..tostring( self.dev:isOpen() ) )
+end
+
+function I2c:close()
+    self.dev:close()
+    print( "closed" )
+end
+
+function I2c:enable()
+    self.dev:i2cEn( 0, true )
+    print( "enabled" )
+end
+
+function I2c:disable()
+    self.dev:i2cEnable( 0, false )
+end
+
+function I2c:config()
+    self.dev:i2cConfig( 0, true, 0, 100 )
+end
+
+function I2c:send01()
+    local addr = 0
+    local st = self.dev:i2cStatus( 0 )
+    print( "status = " .. tostring( st ) )
+    self.dev:i2cSend( 0, 0xA0 + addr, {0}, 1 )
+    for i=1, 10 do
+        st = self.dev:i2cStatus( 0 )
+        print( "status = " .. tostring( st ) )
     end
+    local res = self.dev:i2cResult( 0, 1 )
+    print( "length: " .. tostring( #res ) )
+    print( "result[1]: " .. tostring( res[1] ) )
 end
 
-function Joystick:closed()
-    j = nil
+function I2c:send02()
+    print( "send02()" )
 end
 
-if ( j ) then
-    j = nil
+function I2c:send03()
+    print( "send03()" )
+end
+
+if ( i2c ) then
+    i2c = nil
     collectgarbage()
 end
-j = Joystick()
-while ( true ) do
-    j:refresh()
-end
+i2c = I2c()
+
 
 
 
